08d52a78326c7010745a6252fa83952a4d5d80b0
hamza herbou <hamza95herbou@gmail.com>
2023-01-11 11:31:05 +0100

    Update ButtonDoubleClickListener.cs

--- Assets/ButtonEvents/ButtonDoubleClickListener.cs
+++ Assets/ButtonEvents/ButtonDoubleClickListener.cs
@@ -1,52 +1,48 @@
-﻿/*--------------------------------------
+/*--------------------------------------
    Email  : hamza95herbou@gmail.com
    Github : https://github.com/herbou
 ----------------------------------------*/
 
-using UnityEngine ;
-using UnityEngine.Events ;
-using UnityEngine.EventSystems ;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI ;
 
 [RequireComponent (typeof(Button))]
 public class ButtonDoubleClickListener : MonoBehaviour,IPointerClickHandler {
    
-   [Tooltip ("Max duration between 2 clicks in seconds")]
+   [Tooltip ("Duration between 2 clicks in seconds")]
    [Range (0.01f, 0.5f)] public float doubleClickDuration = 0.4f ;
    public UnityEvent onDoubleClick ;
 
-   private byte clicks = 0 ;
-   private float elapsedTime = 0f ;
+   private byte clicks = 0;
+    private DateTime firstClickTime;
 
-   private Button button ;
+    private Button button;
 
-   private void Awake () {
+
+
+    private void Awake () {
       button = GetComponent<Button> () ;
-   }
-
-   private void Update () {
-      if (clicks == 1) {
-         elapsedTime += Time.deltaTime ;
-         if (elapsedTime > doubleClickDuration) {
-            clicks = 0 ;
-            elapsedTime = 0f ;
-         }
-      }
-   }
+    }
 
    public void OnPointerClick (PointerEventData eventData) {
-      clicks++ ;
+        double elapsedSeconds = (DateTime.Now - firstClickTime).TotalSeconds;
+        if (elapsedSeconds > doubleClickDuration)
+            clicks = 0;
 
-      if (clicks == 1)
-         elapsedTime = 0f ;
-      else if (clicks > 1) {
-         if (elapsedTime <= doubleClickDuration) {
-            clicks = 0 ;
-            elapsedTime = 0f ;
-            if (button.interactable && !object.ReferenceEquals (onDoubleClick, null))
-               onDoubleClick.Invoke () ;
-         }
-      }
+        clicks++ ;
+
+        if (clicks == 1)
+            firstClickTime = DateTime.Now;
+        else if (clicks > 1) {
+            if (elapsedSeconds <= doubleClickDuration) {
+                if (button.interactable)
+                    onDoubleClick?.Invoke();
+            }
+            clicks = 0;
+        }
    }
 
 }
